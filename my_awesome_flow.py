4793ace688c4d8371f640fb4140d016674b43cd0
Ivan Sholana <ivansholana@gmail.com>
2025-08-02 10:58:19 +0700

    Refactor logging in my_awesome_flow.py and update prefect.yaml for deployment configuration

--- my_awesome_flow.py
+++ my_awesome_flow.py
@@ -1,21 +1,23 @@
 import requests
-from prefect import task, flow
+from prefect import task, flow, get_run_logger
 
 @task(retries=3, retry_delay_seconds=10)
 def get_cat_fact():
+    logger = get_run_logger()
     response = requests.get("https://catfact.ninja/fact")
-    response.raise_for_status()  # Raises an HTTPError for bad responses
+    response.raise_for_status()
     fact = response.json().get("fact", "No fact found")
-    print(f"Fetched Cat Fact: {fact}")
+    logger.info(f"Fetched Cat Fact: {fact}")
     return fact
 
 @flow(name="Cat Fact Flow")
 def cat_fact_flow(count: int = 1):
-    print(f"Starting Cat Fact Flow with {count} iterations.")
+    logger = get_run_logger()
+    logger.info(f"Starting Cat Fact Flow with {count} iterations.")
     for _ in range(count):
         fact = get_cat_fact()
-        print(f"Cat Fact: {fact}")
-    print("Cat Fact Flow completed.")
-    
+        logger.info(f"Cat Fact: {fact}")
+    logger.info("Cat Fact Flow completed.")
+
 if __name__ == "__main__":
-    cat_fact_flow(count=3)
+    cat_fact_flow(count=5)
